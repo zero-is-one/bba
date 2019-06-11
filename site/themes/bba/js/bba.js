739163e57d355a335d4812ed2f4f2cdf69ac33b5
Matt Lee <mattjohnlee@github.com>
2019-06-10 20:44:02 -0700

    prevent widows in inline text

--- site/themes/bba/js/bba.js
+++ site/themes/bba/js/bba.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {   
 	console.log('why hello there');
+
+  // prevent widows within inline-text
+
+  $('.inline-text p, .inline-text li').each(function(){
+    var string = $(this).html();
+    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+    $(this).html(string);
+  });
+
 }); 
 //# sourceMappingURL=bba.js.map
--- site/themes/bba/js/main.js
+++ site/themes/bba/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {   
 	console.log('why hello there');
+
+  // prevent widows within inline-text
+
+  $('.inline-text p, .inline-text li').each(function(){
+    var string = $(this).html();
+    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+    $(this).html(string);
+  });
+
 }); 
