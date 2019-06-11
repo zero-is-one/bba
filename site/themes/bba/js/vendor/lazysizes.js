f2a7e5bcde1781ecf1da954eb0e99c4df61208b7
Matt Lee <mattjohnlee@github.com>
2019-06-10 21:13:18 -0700

    add lazyloading

--- site/themes/bba/js/vendor/lazysizes.js
+++ site/themes/bba/js/vendor/lazysizes.js
@@ -701,17 +701,3 @@
 	return lazysizes;
 }
 ));
-
-$(document).ready(function() {   
-	console.log('why hello there');
-
-  // prevent widows within inline-text
-
-  $('.inline-text p, .inline-text li').each(function(){
-    var string = $(this).html();
-    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
-    $(this).html(string);
-  });
-
-}); 
-//# sourceMappingURL=bba.js.map
