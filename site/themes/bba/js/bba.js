f0556bd1ed0b253eceb7dbe4d0e6806d60989694
Matt Lee <mattjohnlee@github.com>
2019-06-13 16:09:58 -0700

    clean up js

--- site/themes/bba/js/bba.js
+++ site/themes/bba/js/bba.js
@@ -4727,14 +4727,14 @@ if(window.jQuery && window.fullpage){
 $(document).ready(function() {   
 	console.log('why hello there');
 
-  // prevent widows within inline-text
+	// prevent widows within inline-text
+	$('.inline-text p, .inline-text li').each(function(){
+		var string = $(this).html();
+		string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+		$(this).html(string);
+	});
 
-  $('.inline-text p, .inline-text li').each(function(){
-    var string = $(this).html();
-    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
-    $(this).html(string);
-  });
-
+	// configure FullPage: https://github.com/alvarotrigo/fullPage.js
 	$('#fullpage').fullpage({
 		//options here
 		lazyLoading: false,
