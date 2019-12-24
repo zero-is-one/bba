c125739b6f05d2ce12a00244d87890d02b054de6
Matt Lee <mattjohnlee@github.com>
2019-12-24 18:18:47 -0500

    scope video.js to the homepage

--- site/themes/bba/js/vendor/videos.js
+++ site/themes/bba/js/vendor/videos.js
@@ -1,62 +1,70 @@
-var videoContainer = document.getElementById('homepage-video'),
-    nextVideo,
-    videoObjects =
-    [
-        document.createElement('video'),
-        document.createElement('video')
-    ],
+( function() {
+	if ( $( "#homepage-video" ).length ) {
 
-    vidSources = [],
-    //random starting point
-    nextActiveVideo = Math.floor((Math.random() * vidSources.length));
+		var videoContainer = document.getElementById('homepage-video'),
+		    nextVideo,
+		    videoObjects =
+		    [
+		        document.createElement('video'),
+		        document.createElement('video')
+		    ],
 
-$('.homepage-video__playlist')
-    .find('a[href]')  // only target <a>s which have a href attribute
-        .each(function() {
-            vidSources.push(this.href);
-        })
-    .end()
-;
+		    vidSources = [],
+		    //random starting point
+		    nextActiveVideo = Math.floor((Math.random() * vidSources.length));
 
-videoObjects[0].inx = 0; //set index
-videoObjects[1].inx = 1;
+		$('.homepage-video__playlist')
+		    .find('a[href]')  // only target <a>s which have a href attribute
+		        .each(function() {
+		            vidSources.push(this.href);
+		        })
+		    .end()
+		;
 
-initVideoElement(videoObjects[0]);
-initVideoElement(videoObjects[1]);
+		videoObjects[0].inx = 0; //set index
+		videoObjects[1].inx = 1;
 
-videoObjects[0].autoplay = 'autoplay';
-videoObjects[0].src = vidSources[nextActiveVideo];
-videoContainer.appendChild(videoObjects[0]);
+		initVideoElement(videoObjects[0]);
+		initVideoElement(videoObjects[1]);
 
-videoObjects[1].style.display = 'none';
-videoContainer.appendChild(videoObjects[1]);
+		videoObjects[0].autoplay = 'autoplay';
+		videoObjects[0].src = vidSources[nextActiveVideo];
+		videoContainer.appendChild(videoObjects[0]);
 
-function initVideoElement(video)
-{
-    video.playsinline = true;
-    video.muted = true;
-    video.preload = 'auto'; //but do not set autoplay, because it deletes preload
+		videoObjects[1].style.display = 'none';
+		videoContainer.appendChild(videoObjects[1]);
 
-    //loadedmetadata is wrong because if we use it then we get endless loop
-    video.onplaying = function(e)
-    {
-        //select next index. If is equal vidSources.length then it is 0
-        nextActiveVideo = ++nextActiveVideo % vidSources.length;
+		function initVideoElement(video)
+		{
+		    video.playsinline = true;
+		    video.muted = true;
+		    video.preload = 'auto'; //but do not set autoplay, because it deletes preload
 
-        //replace the video elements against each other:
-        if(this.inx == 0)
-            nextVideo = videoObjects[1];
-        else
-            nextVideo = videoObjects[0];
+		    //loadedmetadata is wrong because if we use it then we get endless loop
+		    video.onplaying = function(e)
+		    {
+		        //select next index. If is equal vidSources.length then it is 0
+		        nextActiveVideo = ++nextActiveVideo % vidSources.length;
 
-        nextVideo.src = vidSources[nextActiveVideo];
-        nextVideo.pause();
-    };
+		        //replace the video elements against each other:
+		        if(this.inx == 0)
+		            nextVideo = videoObjects[1];
+		        else
+		            nextVideo = videoObjects[0];
 
-    video.onended = function(e)
-    {
-        this.style.display = 'none';
-        nextVideo.style.display = 'block';
-        nextVideo.play();
-    };
-}
+		        nextVideo.src = vidSources[nextActiveVideo];
+		        nextVideo.pause();
+		    };
+
+		    video.onended = function(e)
+		    {
+		        this.style.display = 'none';
+		        nextVideo.style.display = 'block';
+		        nextVideo.play();
+		    };
+		}
+
+
+	}
+
+} () );
